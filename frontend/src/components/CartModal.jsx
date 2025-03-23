fc75d5fee22773d37bea819f1e88b67c83e9181a
alvindioprakosa <alvindioprakosa@gmail.com>
2025-03-23 23:33:45 +0700

    Update Cart Model

--- frontend/src/components/CartModal.jsx
+++ frontend/src/components/CartModal.jsx
@@ -1,23 +1,28 @@
-import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
-import PropTypes from "prop-types";
-import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
-import { Form } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { Button, Modal, Form } from "react-bootstrap";
+import PropTypes from "prop-types";
 import { delCart, updCart } from "../features/CartSlice.js";
 import Swal from "sweetalert2";
-const CartModal = (props) => {
+
+const CartModal = ({ onHide }) => {
   const dispatch = useDispatch();
   const dataEdit = useSelector((state) => state.cart.dataEdit);
-  const [data, setData] = useState({});
+  const [data, setData] = useState({ qty: 0, note: "", id: null, name: "" });
 
-  const updateData = () => {
+  useEffect(() => {
+    if (dataEdit) {
+      setData({ ...dataEdit });
+    }
+  }, [dataEdit]);
+
+  const handleUpdate = () => {
     dispatch(updCart(data));
-    props.onHide();
+    onHide();
     Swal.fire("Update Success!", "", "success");
   };
 
-  const deleteData = (id) => {
+  const handleDelete = () => {
     Swal.fire({
       title: "Are you sure?",
       text: "You won't be able to revert this!",
@@ -28,77 +33,55 @@ const CartModal = (props) => {
       confirmButtonText: "Yes, delete it!",
     }).then((result) => {
       if (result.isConfirmed) {
-        dispatch(delCart(id));
-        props.onHide();
-        Swal.fire("Deleted!", "Your file has been deleted.", "success");
-      } else {
-        return false;
+        dispatch(delCart(data.id));
+        onHide();
+        Swal.fire("Deleted!", "Your item has been deleted.", "success");
       }
     });
   };
 
-  useEffect(() => {
-    setData(dataEdit);
-  }, [dataEdit]);
   return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
+    <Modal show centered onHide={onHide} size="lg">
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Edit {data ? data.name : ""}
-        </Modal.Title>
+        <Modal.Title>Edit {data.name || ""}</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <Form>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Group className="mb-3">
             <Form.Label>Jumlah</Form.Label>
             <Form.Control
-              type="text"
-              placeholder=""
-              value={data ? data.qty : 0}
-              onChange={(e) => setData({ ...data, qty: e.target.value })}
+              type="number"
+              min="0"
+              value={data.qty}
+              onChange={(e) => setData({ ...data, qty: Number(e.target.value) || 0 })}
             />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Group className="mb-3">
             <Form.Label>Keterangan</Form.Label>
             <Form.Control
               as="textarea"
               rows={3}
-              value={data ? data.note : ""}
+              value={data.note}
               onChange={(e) => setData({ ...data, note: e.target.value })}
             />
           </Form.Group>
         </Form>
       </Modal.Body>
       <Modal.Footer>
-        <Button
-          onClick={() => {
-            props.onHide(), updateData();
-          }}
-          variant="success"
-        >
+        <Button onClick={handleUpdate} variant="success">
           Update
         </Button>
-        <Button
-          onClick={() => {
-            props.onHide(), deleteData(data.id);
-          }}
-          variant="danger"
-        >
+        <Button onClick={handleDelete} variant="danger">
           Delete
         </Button>
-        <Button onClick={props.onHide}>Close</Button>
+        <Button onClick={onHide}>Close</Button>
       </Modal.Footer>
     </Modal>
   );
 };
 
 CartModal.propTypes = {
-  onHide: PropTypes.func,
+  onHide: PropTypes.func.isRequired,
 };
 
 export default CartModal;
