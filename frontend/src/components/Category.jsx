cfa3516b76c7ba9f38a8ee5ad3a6a5005b9df412
alvindioprakosa <alvindioprakosa@gmail.com>
2025-03-23 23:33:03 +0700

    Update category

--- frontend/src/components/Category.jsx
+++ frontend/src/components/Category.jsx
@@ -1,10 +1,7 @@
 import { Col, ListGroup } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  categorySelectors,
-  getAllCategory,
-} from "../features/CategorySlice.js";
-import { useEffect, useState } from "react";
+import { categorySelectors, getAllCategory } from "../features/CategorySlice.js";
+import { useEffect, useRef } from "react";
 import { IoFastFoodSharp } from "react-icons/io5";
 import { FaUtensils } from "react-icons/fa";
 import { CiCoffeeCup } from "react-icons/ci";
@@ -14,78 +11,63 @@ import { getProduct, getProductByCategory } from "../features/ProductSlice.js";
 const Category = () => {
   const dispatch = useDispatch();
   const category = useSelector(categorySelectors.selectAll);
-  const [loading, setLoading] = useState(true);
+  const activeRef = useRef(null);
 
   useEffect(() => {
     dispatch(getAllCategory());
   }, [dispatch]);
 
-  useEffect(() => {
-    dispatch(getAllCategory()).finally(() => setLoading(false));
-  }, [dispatch]);
-
-  function setActive(elem) {
-    var a = document.getElementsByClassName("active");
-    for (let i = 0; i < a.length; i++) {
-      a[i].classList.remove("active");
+  const setActive = (elem) => {
+    if (activeRef.current) {
+      activeRef.current.classList.remove("active");
     }
     elem.classList.add("active");
-  }
+    activeRef.current = elem;
+  };
 
-  const setIcon = (categori) => {
-    if (categori == 1) {
-      return <FaUtensils />;
-    } else if (categori == 2) {
-      return <CiCoffeeCup />;
-    } else {
-      return <TbBrandCakephp />;
+  const setIcon = (categoryId) => {
+    switch (categoryId) {
+      case 1:
+        return <FaUtensils />;
+      case 2:
+        return <CiCoffeeCup />;
+      default:
+        return <TbBrandCakephp />;
     }
   };
 
-  const showAll = () => {
-    dispatch(getProduct());
-  };
-
-  const categoryClicked = (id) => {
-    dispatch(getProductByCategory(id));
-  };
   return (
-    <>
-      <Col md={2}>
-        <h4>Product Kategori</h4>
-        <p>{loading ? "Loading..." : ""}</p>
-        <hr />
-        <ListGroup key="all001">
+    <Col md={2}>
+      <h4>Product Kategori</h4>
+      <hr />
+      <ListGroup>
+        <ListGroup.Item
+          id="all001"
+          className="mb-1 shadow-sm active"
+          action
+          ref={activeRef}
+          onClick={(e) => {
+            setActive(e.currentTarget);
+            dispatch(getProduct());
+          }}
+        >
+          <IoFastFoodSharp /> All Product
+        </ListGroup.Item>
+        {category.map((item) => (
           <ListGroup.Item
-            id={`all001`}
+            key={item.id}
             className="mb-1 shadow-sm"
-            active
             action
-            onClick={() => {
-              setActive(document.getElementById(`all001`)), showAll();
+            onClick={(e) => {
+              setActive(e.currentTarget);
+              dispatch(getProductByCategory(item.id));
             }}
           >
-            <IoFastFoodSharp /> All Product
+            {setIcon(item.id)} {item.name}
           </ListGroup.Item>
-        </ListGroup>
-        {category &&
-          category.map((item) => (
-            <ListGroup key={item.id}>
-              <ListGroup.Item
-                id={`key${item.id}`}
-                className="mb-1 shadow-sm"
-                action
-                onClick={() => {
-                  setActive(document.getElementById(`key${item.id}`)),
-                    categoryClicked(item.id);
-                }}
-              >
-                {setIcon(item.id)} {item.name}
-              </ListGroup.Item>
-            </ListGroup>
-          ))}
-      </Col>
-    </>
+        ))}
+      </ListGroup>
+    </Col>
   );
 };
 
