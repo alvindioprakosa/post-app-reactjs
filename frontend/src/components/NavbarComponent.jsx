8bc4b67d4bdb61366d3b935a958e4c858512b5b2
alvindioprakosa <alvindioprakosa@gmail.com>
2025-03-23 23:33:18 +0700

    Update Navbar

--- frontend/src/components/NavbarComponent.jsx
+++ frontend/src/components/NavbarComponent.jsx
@@ -1,26 +1,34 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+
 const NavbarComponent = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const masterItems = [
+    { path: "/action1", label: "Action" },
+    { path: "/action2", label: "Another action" },
+    { path: "/action3", label: "Something" },
+    { path: "/action4", label: "Separated link", divider: true },
+  ];
+
   return (
-    <Navbar expand="lg" className="bg-body-tertiary shadow-sm">
+    <Navbar expand="lg" className="bg-body-tertiary shadow-sm" expanded={expanded}>
       <Container fluid>
-        <Navbar.Brand href="#home">Kasir</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Brand as={Link} to="/" onClick={() => setExpanded(false)}>Kasir</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(!expanded)} />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link as={Link} to="/" onClick={() => setExpanded(false)}>Home</Nav.Link>
             <NavDropdown title="Master" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+              {masterItems.map((item, index) => (
+                <div key={index}>
+                  {item.divider && <NavDropdown.Divider />}
+                  <NavDropdown.Item as={Link} to={item.path} onClick={() => setExpanded(false)}>
+                    {item.label}
+                  </NavDropdown.Item>
+                </div>
+              ))}
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
