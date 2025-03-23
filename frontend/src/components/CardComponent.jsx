2fe3da8a62a90ff1f7d25979f70fe2aafde5f0db
alvindioprakosa <alvindioprakosa@gmail.com>
2025-03-23 23:33:32 +0700

    Update Card Component

--- frontend/src/components/CardComponent.jsx
+++ frontend/src/components/CardComponent.jsx
@@ -1,23 +1,35 @@
 import { Card, Col } from "react-bootstrap";
 import PropTypes from "prop-types";
 
-const CardComponent = ({ product, setCart }) => {
-  return (
-    <Col md={4} xs={6} className="mb-4">
-      <Card className="shadow" onClick={() => setCart(product)}>
-        <Card.Img variant="top" src={"/img/" + product.image} />
-        <Card.Body>
-          <Card.Title>{product.name + " (" + product.code + ")"}</Card.Title>
-          <Card.Text>Rp. {product.price.toLocaleString("id-ID")}</Card.Text>
-        </Card.Body>
-      </Card>
-    </Col>
-  );
-};
+const CardComponent = ({ product: { image, name, code, price }, setCart }) => (
+  <Col md={4} xs={6} className="mb-4">
+    <Card className="shadow" onClick={() => setCart({ image, name, code, price })}>
+      <Card.Img variant="top" src={`/img/${image}`} />
+      <Card.Body>
+        <Card.Title>{`${name} (${code})`}</Card.Title>
+        <Card.Text>Rp. {price.toLocaleString("id-ID")}</Card.Text>
+      </Card.Body>
+    </Card>
+  </Col>
+);
 
 CardComponent.propTypes = {
-  product: PropTypes.object,
-  setCart: PropTypes.func,
+  product: PropTypes.shape({
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    code: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+  }),
+  setCart: PropTypes.func.isRequired,
+};
+
+CardComponent.defaultProps = {
+  product: {
+    image: "",
+    name: "Unknown Product",
+    code: "N/A",
+    price: 0,
+  },
 };
 
 export default CardComponent;
