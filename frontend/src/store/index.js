3cbfcee7e386a2e9b320f89270c6cfdc7c7a337d
alvindioprakosa <alvindioprakosa@gmail.com>
2025-05-09 16:49:49 +0700

    Update Store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,12 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import categoryReducer from "../features/CategorySlice.js";
-import productReducer from "../features/ProductSlice.js";
-import cartReducer from "../features/CartSlice.js";
+import categoryReducer from "../features/CategorySlice";
+import productReducer from "../features/ProductSlice";
+import cartReducer from "../features/CartSlice";
 
+// Konfigurasi store global
 export const store = configureStore({
   reducer: {
     category: categoryReducer,
     product: productReducer,
     cart: cartReducer,
   },
+  // middleware tambahan bisa disisipkan di sini jika diperlukan
+  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(customMiddleware),
 });
